ae685268f46084e1712344e2f9eec598906fc67f
Jonathan Edwards <jondedwards13@gmail.com>
2022-05-29 14:12:58 -0500

    Adding some starter code

---
+++ script.js
@@ -0,0 +1,53 @@
+const $timer = document.getElementById("timer");
+const $start = document.getElementById("start");
+const $quiz = document.getElementById("quiz");
+const $score = document.getElementById("score");
+let interval
+let countdown = 10
+const quizArr = [
+    {
+        question:"The first question",
+        answer:["answer 1","answer 2","answer 3", "answer 4"],
+        answerKey:"answer 1",
+    },
+    
+]
+const startQuizHandler =(event) => {
+console.log(event)
+startTimer()
+}
+const startTimer =() => {
+interval = setInterval(() => {
+    countdown-- 
+    if (countdown <= 0){
+        endGame()
+    }
+    $timer.textContent=countdown
+},1000)
+}
+
+const endGame =() => {
+    clearInterval(interval);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const $startButton = $start.children[0];
+$startButton.addEventListener("click",startQuizHandler)
